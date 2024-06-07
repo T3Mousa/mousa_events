6732604304b96799212981344dc4e934c65e4c02
Tamara <jermanust88@gmail.com>
2024-06-06 21:56:36 -0400

    added package.json to root directory

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const path = require('path')
 const apiRouter = require('./api');
 
 const router = express.Router();
 
 router.use('/api', apiRouter)
+
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    router.get('/', (req, res) => {
+        // res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+        );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/dist")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        // res.cookie('XSRF-TOKEN', req.csrfToken());
+        res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+        );
+    });
+};
 
 // Add a XSRF-TOKEN cookie in development
 // if (process.env.NODE_ENV !== 'production') {
